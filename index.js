33db7beb9d3748a2ee6068943e2fb12cb9b8cfd5
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-09 23:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective() {
+function wrapAdjective(highlight = "*") {
   return function(param = "special") {
-    return `You are ${param}!`
+    return `You are ${highlight}${param}${highlight}!`
   }
 }
