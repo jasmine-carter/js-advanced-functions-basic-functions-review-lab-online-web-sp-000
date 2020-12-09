52345b2de53a066f882e06b41b5b3555ddbe551f
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-09 23:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,11 @@ const Calculator = {
   },
   subtract: function(num1, num2){
     return num1 - num2
+  },
+  multiply: function(num1, num2){
+    return num1 * num2
+  },
+  divide: function(num1, num2){
+    return num1 / num2
   }
 }
