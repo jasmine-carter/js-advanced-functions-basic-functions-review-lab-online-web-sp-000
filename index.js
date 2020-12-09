fb1e85baef187a232d29ebda1a9873e4657bd123
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-09 23:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,8 @@ function wrapAdjective(highlight = "*") {
 const Calculator = {
   add: function(num1, num2){
     return num1 + num2
+  },
+  subtract: function(num1, num2){
+    return num1 - num2
   }
 }
